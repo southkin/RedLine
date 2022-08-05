fc6f70e9341730043c61660bb752a47e8edd06a9
kin <south.kin@gmail.com>
2022-08-05 13:56:27 +0900

    swiftfpm

---
+++ Tests/RedLineTests/RedLineTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RedLine
+
+final class RedLineTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RedLine().text, "Hello, World!")
+    }
+}
